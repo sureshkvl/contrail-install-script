5140f0df59b0c9d1d7ba8b2850654d5743956f40
Cloud user <cloud@aio3.novalocal>
2019-07-26 08:30:52 +0000

    updated aio_insall.sh

--- allinone/aio_install.sh
+++ allinone/aio_install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 HOMEDIR=/home/cloud
-HOSTIP=10.1.1.1
+HOSTIP=10.0.1.5
+
+VIF=/home/cloud/.nix-profile/bin/vif
+PROVISION=/home/cloud/.nix-profile/bin/provision_vgw_interface.py
+
+
+#Global variables
+VHOST_INTERFACE_NAME=ens3
+VHOST_INTERFACE_CIDR=10.0.1.5/24
+VHOST_INTERFACE_IP=10.0.1.5
+DEFAULT_GW=10.0.1.1
+
+FLOATING_RANGE=${FLOATING_RANGE:-172.24.4.0/24}
+Q_L3_ENABLED=${Q_L3_ENABLED:-True}
+VGW_MASQUERADE=${VGW_MASQUERADE:-True}
+VR_KMOD_OPTS=${VR_KMOD_OPTS:-"vr_flow_entries=4096 vr_oflow_entries=512 vr_bridge_entries=128"}
+
 
 
 function setup_infra_services() {
@@ -37,10 +53,26 @@ function configure_rabbitmq() {
 }
 
 
+function check_infra_services() {
+        #creating rabbit mq users
+        echo "Checking REDIS Process status....."
+        ps -ef |grep redis
+        echo "Checking ZOOKEEPER Process status....."
+        ps -ef |grep zookeeper
+        echo "Checking RABBITMQ Process status....."
+        ps -ef |grep rabbitmq
+        echo "Checking CASSANDRA Process status....."
+        ps -ef |grep cassandra
+
+}
+
+
+
+
 function install_contrail(){
 
 	cd $HOMEDIR
-	git clone https://github.com/cloudwatt/nixpkgs-tungsten
+	git clone https://github.com/sureshkvl/nixpkgs-tungsten
 	cd nixpkgs-tungsten
 	./please init
 
@@ -112,130 +144,128 @@ function copy_config(){
 	fi
 	
 	# create plugins folder
-	if [ ! -d "/var/log/contrail" ];
+	if [ ! -d "/etc/neutron/plugins/opencontrail" ];
 	then	
 		sudo mkdir /etc/neutron/plugins/opencontrail
 	fi
 
 	#copy the config files
 	sudo cp $HOMEDIR/contrail-install-script/allinone/config/* /etc/contrail/.
-    sudo cp $HOMEDIR/contrail-install-script/allinone/config/ContrailPlugin.ini /etc/neutron/plugins/opencontrail/.
+        sudo cp $HOMEDIR/contrail-install-script/allinone/config/ContrailPlugin.ini /etc/neutron/plugins/opencontrail/.
 	sudo cp $HOMEDIR/contrail-install-script/allinone/systemctl/* /etc/systemd/system/.
 
 }
 
-
-function update_config{
-	# modify the contrail config files
-	# modify the sysemctl(if ??)
+function verify_services(){
+       echo "checking the contrail services"
 }
 
-function restart_contrail_services{
+function restart_contrail_services(){
 	# reload he systemctl
 	sudo systemctl daemon-reload
 
 	#create config file folder
 	# to
-    sudo systemctl start contrail@api
-    sudo systemctl start contrail@schema
-    sudo systemctl start contrail@svc-monitor
-    sudo systemctl start contrail@control
-    sudo systemctl start contrail@analytics-api
-    sudo systemctl start contrail@query-engine
-    sudo systemctl start contrail@collector  
+        sudo systemctl start contrail@api
+        sudo systemctl start contrail@schema
+        sudo systemctl start contrail@svc-monitor
+        sudo systemctl start contrail@control
+        sudo systemctl start contrail@analytics-api
+        sudo systemctl start contrail@query-engine
+        sudo systemctl start contrail@collector  
+}
+
+function cleanup_ovs_footprints(){
+	sudo ip netns del $(sudo ip netns ls)
+	sudo ip netns del $(sudo ip netns ls)
+
+	#remove the ovs bridges
+	sudo ifconfig br-int down
+	sudo ifconfig br-ex down
+	sudo ifconfig br-tun down
+
+	sudo ovs-vsctl del-br br-int
+	sudo ovs-vsctl del-br br-ex
+	sudo ovs-vsctl del-br br-tun
+	sudo service openvswitch-switch stop
+
+}
+
+function build_vrouter(){
+        cd $HOMEDIR/nixpkgs-tungsten
+	#Build and load the vrouter kernel module,its available only in my sureshkvl repo
+	./please build contrail50.vrouterModuleUbuntu_4_4_0_119_generic
+	sudo cp ./result/lib/modules/4.4.0-119-generic/extra/net/vrouter/vrouter.ko  /lib/modules/4.4.0-119-generic/.
+	sudo insmod ./result/lib/modules/4.4.0-119-generic/extra/net/vrouter/vrouter.ko  $VR_KMOD_OPTS
+	sudo lsmod |grep vrouter
+}
+
+
+# copied from https://github.com/zioc/contrail-devstack-plugin/blob/master/devstack/plugin.sh
+function setup_vrouter(){
+
+    if ! lsmod | grep -q vrouter; then
+        echo "Inserting vrouter kernel module"
+        #sudo modprobe vrouter $VR_KMOD_OPTS
+        sudo insmod /lib/modules/4.4.0-119-generic/vrouter.ko $VR_KMOD_OPTS
+        if [[ ! $? -eq 0 ]]; then
+            echo "Failed to insert vrouter kernel module"
+            return 1
+        fi
+    fi
+
+    #Check if vrouter interface have already been added
+    if ip link show |grep -q vhost0; then
+        return 0
+    fi
+
+    DEV_MAC=$(cat /sys/class/net/$VHOST_INTERFACE_NAME/address)
+
+    sudo $VIF --create vhost0 --mac $DEV_MAC
+    sudo $VIF --add $VHOST_INTERFACE_NAME --mac $DEV_MAC --vrf 0 --vhost-phys --type physical
+    sudo $VIF --add vhost0 $DEVICE --mac $DEV_MAC --vrf 0 --xconnect $VHOST_INTERFACE_NAME --type vhost
+
+    sudo ip link set vhost0 up
+    sudo ip addr add $VHOST_INTERFACE_CIDR dev vhost0
+    # Migrate routes to vhost0
+    sudo ip route show dev $VHOST_INTERFACE_NAME scope global | while read route; do
+        sudo ip route replace $route dev vhost0 || true
+    done
+    sudo ip addr flush dev $VHOST_INTERFACE_NAME
+}
+
+
+function configure_neutron() {
+        #to be relooked...dirty code
+        sudo sed -i 's/service_plugins = neutron.services.l3_router.l3_router_plugin.L3RouterPlugin/api_extensions_path = extensions:\/usr\/local\/lib\/python2.7\/dist-packages\/neutron_plugin_contrail\/extensions/' /etc/neutron/neutron.conf
+        sudo sed -i 's/core_plugin = ml2/core_plugin = neutron_plugin_contrail.plugins.opencontrail.contrail_plugin.NeutronPluginContrailCoreV2/' /etc/neutron/neutron.conf
+        sudo sed -i 's/signing_dir/#signing_dir/' /etc/neutron/neutron.conf
+        sudo sed -i 's/cafile/#cafile/' /etc/neutron/neutron.conf
 }
 
 
 
 
-#step1
-setup_infra_services
-configure_rabbitmq
-configure_redis
-configure_cassandra
+
+
+
+#step1 - infra services
+#======================
+#setup_infra_services
+#configure_rabbitmq
+#configure_redis
+#configure_cassandra
+#check_infra_services
+
+#step2 - contrail config,contol,analytics
+#=======================================
 #install_contrail
 #copy_config
-#update_config
 #restart_contrail_services
-#verify_serices
 
-
-
-
-:'
-# 
-cd
-git clone https://github.com/cloudwatt/nixpkgs-tungsten
-cd nixpkgs-tungsten
-./please init
-
-#souring nix profile
-. /home/cloud/.nix-profile/etc/profile.d/nix.sh
-#contrail config 
-./please install contrail50.apiServer
-./please install contrail50.schemaTransformer
-./please install contrail50.svcMonitor
-
-#contrail control
-./please install contrail50.control
-
-#contrail analytics
-./please install contrail50.analyticsApi
-./please install contrail50.collector
-nix-env -f default.nix --set-flag priority 6 contrail-control-5.0
-./please install contrail50.collector
-./please install contrail50.queryEngine
-nix-env -f default.nix --set-flag priority 7 contrail-collector-5.0
-./please install contrail50.queryEngine
-
-
-
-
-
-./please build contrail50.pythonPackages.vnc_api
-sudo cp ./result/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages/. -rf
-
-./please build contrail50.pythonPackages.cfgm_common
-sudo cp ./result/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages/. -rf
-
-./please build contrail50.pythonPackages.contrail_vrouter_api
-sudo cp ./result/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages/. -rf
-
-./please build contrail50.pythonPackages.vnc_openstack
-sudo cp ./result/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages/. -rf
-
-
-#install contraill api cli
-./please install contrailApiCliWithExtra
-
-# provisioning scripts
-./please install contrail50.configUtils
-
-
-#create config files
-sudo mkdir /etc/contrail
-cd
-sudo cp contrail-install-script/contrail/config/* /etc/contrail/.
-
-#create log folder
-sudo mkdir /var/log/contrail
-sudo chmod 777 /var/log/contrail
-
-#copy the systemd files
-cd
-sudo cp contrail-install-script/contrail/systemctl/* /etc/systemd/system/.
-
-# reload he systemctl
-sudo systemctl daemon-reload
-
-#start contrail services
-#sudo systemctl start contrail@api
-#sudo systemctl start contrail@schema
-#sudo systemctl start contrail@svc-monitor
-#sudo systemctl start contrail@control
-#sudo systemctl start contrail@analytics-api
-#sudo systemctl start contrail@collector
-#sudo systemctl start contrail@query-engine
-
-
-'
+#step3 - vrouter
+#=================
+#cleanup_ovs_footprints
+#build_vrouter
+#setup_vrouter
+#configure_neutron
---
+++ allinone/aio_provision.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+
+DEVSTACKDIR=/home/cloud/devstack
+VIF=/home/cloud/.nix-profile/bin/vif
+PROVISION=/home/cloud/.nix-profile/bin/provision_vgw_interface.py
+
+
+#Global variables
+VHOST_INTERFACE_NAME=ens3
+VHOST_INTERFACE_CIDR=10.0.1.5/24
+VHOST_INTERFACE_IP=10.0.1.5
+DEFAULT_GW=10.0.1.1
+
+FLOATING_RANGE=${FLOATING_RANGE:-172.24.4.0/24}
+Q_L3_ENABLED=${Q_L3_ENABLED:-True}
+VGW_MASQUERADE=${VGW_MASQUERADE:-True}
+VR_KMOD_OPTS=${VR_KMOD_OPTS:-"vr_flow_entries=4096 vr_oflow_entries=512 vr_bridge_entries=128"}
+
+
+CONTRAIL_HOSTNAME=aio3
+VROUTER_HOSTNAME=aio3
+
+#contrail node ip
+API_SERVER_IP=10.0.1.5
+
+#devstack ip
+VROUTER_IP=10.0.1.5
+NOVA_SERVICE_HOST=10.0.1.5
+
+#VIF=/home/cloud/.nix-profile/bin/vif
+PROVISION_VROUTER=/home/cloud/.nix-profile/bin/provision_vrouter.py
+PROVISION_CONTROL=/home/cloud/.nix-profile/bin/provision_control.py
+PROVISION_LINKLOCAL=/home/cloud/.nix-profile/bin/provision_linklocal.py
+
+
+
+
+
+
+function create_publicnw() {
+    source $DEVSTACKDIR/openrc admin admin
+    neutron net-create public --router:external True --provider:network_type local
+    neutron subnet-create --gateway 172.24.4.1 --allocation-pool start=172.24.4.5,end=172.24.4.50 public 172.24.4.0/24
+}
+
+function setupgw(){
+
+        if [[ "$Q_L3_ENABLED" == "True" ]]; then
+        sudo $PROVISION --oper create \
+            --interface vgw --subnets $FLOATING_RANGE --routes 0.0.0.0/0 \
+            --vrf "default-domain:admin:public:public"
+        if [[ "$VGW_MASQUERADE" == "True" ]] && ! sudo iptables -t nat -C POSTROUTING -s $FLOATING_RANGE -j MASQUERADE > /dev/null 2>&1; then
+            sudo iptables -t nat -A POSTROUTING -s $FLOATING_RANGE -j MASQUERADE
+        fi
+    fi
+}
+
+function provision_vrouter(){
+
+        sudo $PROVISION_VROUTER --api_server_ip $API_SERVER_IP \
+            --api_server_port 8082 --host_ip $VROUTER_IP --host_name VROUTER_HOSTNAME \
+            --oper add 
+        
+        sudo $PROVISION_CONTROL --host_name $CONTRAIL_HOSTNAME --host_ip $API_SERVER_IP \
+             --router_asn 64512  --api_server_ip $API_SERVER_IP --api_server_port 8082 --oper add
+
+        sudo $PROVISION_LINKLOCAL  --api_server_ip $API_SERVER_IP \
+             --oper add --linklocal_service_name metadata --linklocal_service_ip 169.254.169.254 \
+             --linklocal_service_port 80 --ipfabric_service_ip $NOVA_SERVICE_HOST --ipfabric_service_port 8775
+
+
+
+}
+
+#create_publicnw
+#setupgw
+provision_vrouter
