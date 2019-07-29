e0e571922ea37569e33fefcde2cf3c40fd064951
suresh <sureshkumarr.s@gmail.com>
2019-07-29 17:18:38 +0530

    updated with tfstanalone

---
+++ tf-standalone/Readme.md
@@ -0,0 +1,158 @@
+# contrail-install-script
+
+
+## 1. Introduction
+
+This tutorial helps to install tungsten-fabric (a.k.a) opencontrail (using nixpkgs-tungsten repo) and integreate with openstack.
+
+
+This tutorial demonstrates Contrail 5.0 + Openstack Ocata on Ubuntu 16.04 VM.
+
+
+
+## 2. Installation Procedure
+
+Setup:  I am using Ubuntu 16.04 VM (16GB RAM/4 Core Processor:)
+
+
+
+### 1. Devstack installation
+
+**1. Download **
+
+```
+cd 
+git clone https://opendev.org/openstack/devstack
+cd devstack
+git checkout stable/ocata
+```
+
+
+**2. Create local.conf **
+vi local.conf
+
+
+```
+[[local|localrc]]
+RECLONE=True
+HOST_IP=10.0.1.4
+SERVICE_TOKEN=mytoken123
+ADMIN_PASSWORD=openstack123
+MYSQL_PASSWORD=mysql123
+RABBIT_PASSWORD=rabbit123
+SERVICE_PASSWORD=$ADMIN_PASSWORD
+LOGFILE=$DEST/logs/stack.sh.log
+LOGDAYS=2
+disable_service n-net tempest c-api c-sch c-vol
+enable_service q-svc q-agt q-dhcp q-l3 q-meta 
+```
+Note: provide your HOST_IP.
+
+**3.Start the installation**
+
+
+```
+./stack.sh
+```
+
+It will take 15 to 30 mins to complete it.
+
+
+
+**4.Stop the below services**
+
+
+Stop the neutron services (q-svc, q-meta,q-l3, q-dhcp, q-agt) in the screen or systemctl
+
+Stop the nova compute (n-cpu) in the screen or systemctl
+
+
+
+### 2. Contrail Installaion - PART1 
+
+1. Download and run install script 
+
+
+
+```
+git clone https://github.com/sureshkvl/contrail-install-script
+cd contrail-install-script/allinone/
+```
+
+Edit the aio_install.sh , and provide the proper input(IPs, interface, hostname etc)
+
+```
+./aio_install.sh
+```
+
+
+Note: This script installs the infra services(cassandra, rabbitmq, redis) and contrail services(config,control,analyics services) with default configuration.
+
+
+### 2. Contrail Installaion - PART2
+
+**Manual steps**
+
+1.  Edit the cfgm_common package (bug)
+
+- vi #/usr/local/lib/python2.7/dist-packages/cfgm_common/__init__.py
+- add this below line
+```
+SG_NO_RULE_NAME = '__no_rule__'
+```
+
+
+2. Run the neutron with opencontrail plugin conf file in the screen
+
+```
+/usr/local/bin/neutron-server --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/opencontrail/ContrailPlugin.ini & echo $! >/opt/stack/status/stack/q-svc.pid; fg || echo "q-svc failed to start" | tee "/opt/stack/status/stack/q-svc.failure"
+```
+
+
+3. Include the Contrail VIF driver in the nova.conf file 
+
+Edit /etc/nova/nova.conf, under libvirt section include vif_driver as below,
+
+
+```
+[libvirt]
+vif_driver = nova_contrail_vif.contrailvif.VRouterVIFDriver
+```
+
+
+4. Include the nix path in the nova execution search path (/etc/nova/rootwrap.conf)
+edit /etc/nova/rootwrap.conf
+
+Update exec_dirs line with /home/cloud/.nix-profile/bin
+
+```
+exec_dirs=/sbin,/usr/sbin,/bin,/usr/bin,/usr/local/sbin,/usr/local/bin,/home/cloud/.nix-profile/bin
+```
+
+5. Restart the nova compute services.
+
+
+6. Edit the /etc/contrail/contrail-vrouter-agent.conf and provide proper IP deails.
+
+
+7. Start the vrouter agent
+```
+sudo -i
+export LC_ALL="C"; unset LANGUAGE
+/home/cloud/.nix-profile/bin/contrail-vrouter-agent --config_file /etc/contrail/contrail-vrouter-agent.conf
+```
+systemctl doesnt work, need to check.???
+
+
+
+
+### 4. Contrail Installaion - PART3
+
+1. Run Contrail PROVISION script 
+
+Edit the script and update the IPs.
+
+```
+./aio_provision.sh
+```
+
---
+++ tf-standalone/aio_install.sh
@@ -0,0 +1,230 @@
+#!/bin/bash
+
+HOMEDIR=/home/cloud
+HOSTIP=10.0.1.5
+
+
+
+VIF=$HOMEDIR/.nix-profile/bin/vif
+PROVISION=$HOMEDIR/.nix-profile/bin/provision_vgw_interface.py
+
+
+#Global variables
+VHOST_INTERFACE_NAME=ens3
+VHOST_INTERFACE_CIDR=$HOSTIP/24
+VHOST_INTERFACE_IP=$HOSTIP
+DEFAULT_GW=10.0.1.1
+
+FLOATING_RANGE=${FLOATING_RANGE:-172.24.4.0/24}
+Q_L3_ENABLED=${Q_L3_ENABLED:-True}
+VGW_MASQUERADE=${VGW_MASQUERADE:-True}
+VR_KMOD_OPTS=${VR_KMOD_OPTS:-"vr_flow_entries=4096 vr_oflow_entries=512 vr_bridge_entries=128"}
+
+
+
+function setup_infra_services() {
+
+	sudo apt-get update
+	sudo apt-get install -y git
+	#install redis zookeeper rabitmq
+	sudo apt-get install -y redis-server zookeeperd rabbitmq-server
+
+	#install cassandra
+	echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+	curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+	sudo apt-get update
+	sudo apt-get install cassandra -y
+}
+
+
+function configure_cassandra() {
+	sudo sed -i 's/start_rpc: false/start_rpc: true/' /etc/cassandra/cassandra.yaml
+	sudo service cassandra restart
+}
+
+function configure_redis() {
+
+	sudo sed -i 's/bind localhost/bind 0.0.0.0/' /etc/redis/redis.conf
+	sudo service redis-server restart
+}
+
+function configure_rabbitmq() {
+	#creating rabbit mq users
+	sudo rabbitmqctl add_user contrail contrail
+	sudo rabbitmqctl set_permissions -p / contrail ".*" ".*" ".*"
+}
+
+
+function check_infra_services() {
+        #creating rabbit mq users
+        echo "Checking REDIS Process status....."
+        ps -ef |grep redis
+        echo "Checking ZOOKEEPER Process status....."
+        ps -ef |grep zookeeper
+        echo "Checking RABBITMQ Process status....."
+        ps -ef |grep rabbitmq
+        echo "Checking CASSANDRA Process status....."
+        ps -ef |grep cassandra
+
+}
+
+
+
+
+function install_contrail(){
+
+	cd $HOMEDIR
+	git clone https://github.com/sureshkvl/nixpkgs-tungsten
+	cd nixpkgs-tungsten
+	./please init
+
+	#souring nix profile
+	. $HOMEDIR/.nix-profile/etc/profile.d/nix.sh
+	#contrail config 
+	./please install contrail50.apiServer
+	./please install contrail50.schemaTransformer
+	./please install contrail50.svcMonitor
+
+	#contrail control
+	./please install contrail50.control
+
+	#contrail analytics
+	./please install contrail50.analyticsApi
+	./please install contrail50.collector
+	nix-env -f default.nix --set-flag priority 6 contrail-control-5.0
+	./please install contrail50.collector
+	./please install contrail50.queryEngine
+	nix-env -f default.nix --set-flag priority 7 contrail-collector-5.0
+	./please install contrail50.queryEngine
+	nix-env -f default.nix --set-flag priority 8 contrail-query-engine-5.0
+
+
+	#install the vrouter agent
+	./please install contrail50.vrouterAgent
+	./please install contrail50.vrouterNetNs
+	./please install contrail50.vrouterPortControl
+	./please install contrail50.vrouterUtils
+	# provision scripts are available in configutils package
+	./please install contrail50.configUtils
+	#install contraill api cli
+	./please install contrailApiCliWithExtra
+
+
+
+	#install python packages and move it in to python lib folder
+
+	./please build contrail50.pythonPackages.vnc_api
+	sudo cp ./result/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages/. -rf
+
+	./please build contrail50.pythonPackages.cfgm_common
+	sudo cp ./result/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages/. -rf
+
+	./please build contrail50.pythonPackages.contrail_vrouter_api
+	sudo cp ./result/lib/python2.7/site-packages/* /usr/local/lib/python2.7/dist-packages/. -rf
+}
+
+
+function copy_config(){
+	#create config file folder
+
+	if [ ! -d "/etc/contrail" ];
+	then
+		sudo mkdir /etc/contrail
+	fi
+
+	#create log folder
+	if [ ! -d "/var/log/contrail" ];
+	then
+		sudo mkdir /var/log/contrail
+		sudo chmod 777 /var/log/contrail
+	fi
+	
+
+	#copy the config files
+	sudo cp $HOMEDIR/contrail-install-script/allinone/config/* /etc/contrail/.
+	sudo cp $HOMEDIR/contrail-install-script/allinone/systemctl/* /etc/systemd/system/.
+
+}
+
+function verify_services(){
+       echo "checking the contrail services"
+}
+
+function restart_contrail_services(){
+	# reload he systemctl
+	sudo systemctl daemon-reload
+
+	#create config file folder
+	# to
+        sudo systemctl start contrail@api
+        sudo systemctl start contrail@schema
+        sudo systemctl start contrail@svc-monitor
+        sudo systemctl start contrail@control
+        sudo systemctl start contrail@analytics-api
+        sudo systemctl start contrail@query-engine
+        sudo systemctl start contrail@collector  
+}
+
+
+function build_vrouter(){
+    cd $HOMEDIR/nixpkgs-tungsten
+	#Build and load the vrouter kernel module,its available only in my sureshkvl repo
+	./please build contrail50.vrouterModuleUbuntu_4_4_0_119_generic
+	sudo cp ./result/lib/modules/4.4.0-119-generic/extra/net/vrouter/vrouter.ko  /lib/modules/4.4.0-119-generic/.
+	sudo insmod ./result/lib/modules/4.4.0-119-generic/extra/net/vrouter/vrouter.ko  $VR_KMOD_OPTS
+	sudo lsmod |grep vrouter
+}
+
+
+# copied from https://github.com/zioc/contrail-devstack-plugin/blob/master/devstack/plugin.sh
+function setup_vrouter(){
+
+    if ! lsmod | grep -q vrouter; then
+        echo "Inserting vrouter kernel module"
+        #sudo modprobe vrouter $VR_KMOD_OPTS
+        sudo insmod /lib/modules/4.4.0-119-generic/vrouter.ko $VR_KMOD_OPTS
+        if [[ ! $? -eq 0 ]]; then
+            echo "Failed to insert vrouter kernel module"
+            return 1
+        fi
+    fi
+
+    #Check if vrouter interface have already been added
+    if ip link show |grep -q vhost0; then
+        return 0
+    fi
+
+    DEV_MAC=$(cat /sys/class/net/$VHOST_INTERFACE_NAME/address)
+
+    sudo $VIF --create vhost0 --mac $DEV_MAC
+    sudo $VIF --add $VHOST_INTERFACE_NAME --mac $DEV_MAC --vrf 0 --vhost-phys --type physical
+    sudo $VIF --add vhost0 $DEVICE --mac $DEV_MAC --vrf 0 --xconnect $VHOST_INTERFACE_NAME --type vhost
+
+    sudo ip link set vhost0 up
+    sudo ip addr add $VHOST_INTERFACE_CIDR dev vhost0
+    # Migrate routes to vhost0
+    sudo ip route show dev $VHOST_INTERFACE_NAME scope global | while read route; do
+        sudo ip route replace $route dev vhost0 || true
+    done
+    sudo ip addr flush dev $VHOST_INTERFACE_NAME
+}
+
+
+#step1 - infra services
+#======================
+setup_infra_services
+configure_rabbitmq
+configure_redis
+configure_cassandra
+check_infra_services
+
+#step2 - contrail config,contol,analytics
+#=======================================
+install_contrail
+copy_config
+restart_contrail_services
+
+#step3 - vrouter
+#=================
+build_vrouter
+setup_vrouter
